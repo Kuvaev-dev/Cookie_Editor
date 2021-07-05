10b4632d42eac8b6d189c02d2ac1838a7d2f4544
Somewhere <66011798+Kuvaev-dev@users.noreply.github.com>
2021-07-05 20:54:39 +0300

    Something Update

--- Cookie_Editor/Properties/AssemblyInfo.cs
+++ Cookie_Editor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cookie_listener")]
+[assembly: AssemblyTitle("Cookie_Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cookie_listener")]
+[assembly: AssemblyProduct("Cookie_Editor")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
